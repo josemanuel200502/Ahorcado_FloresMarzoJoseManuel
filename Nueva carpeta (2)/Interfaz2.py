a02c3deccfd0e057b2d6f052c0d98621a0ba5c52
JoseManuel <pnp729752@gmail.com>
2024-11-26 19:59:10 +0100

    Onceavo Commit
    Hemos añadido algún que otro comentario al código, hemos reseteado la base de datos y hemos añadido la presentación.

--- Nueva carpeta (2)/Interfaz2.py
+++ Nueva carpeta (2)/Interfaz2.py
@@ -3,9 +3,8 @@ import tkinter as tk
 from tkinter import messagebox
 import sqlite3
 import random
-import datetime
-import keyword
-from PIL import Image,ImageTk
+from PIL import Image, ImageTk
+
 
 class InterfazAhorcado:
     def __init__(self, root):
@@ -16,10 +15,10 @@ class InterfazAhorcado:
         self.palabra_oculta = []
         self.intentos = 6
         self.letras_intentadas = []
-        self.imagen_label=tk.Label(self.root)
+        self.imagen_label = tk.Label(self.root)
         self.imagen_label.pack(pady=10)
-        self.tema_actual=None
-        self.imagenes=[
+        self.tema_actual = None
+        self.imagenes = [
             self.redimensionar_imagen(Image.open("Imagenes/imagen1.png")),
             self.redimensionar_imagen(Image.open("Imagenes/imagen2.png")),
             self.redimensionar_imagen(Image.open("Imagenes/imagen3.png")),
@@ -32,19 +31,30 @@ class InterfazAhorcado:
         ]
 
         # Crear la interfaz de entrada
+
         self.mostrar_entrada_nombre()
 
-    def redimensionar_imagen (self,imagen,tamano=(300,300)):
+
+    # Metodo para redimensionar la imagen
+
+    def redimensionar_imagen(self, imagen, tamano=(300, 300)):
         imagen = imagen.resize(tamano, Image.Resampling.LANCZOS)
         return ImageTk.PhotoImage(imagen)
 
+    # Metodo para conexion a la base de datos
 
     def conectar_db(self):
+
         # Establece la conexión a la base de datos.
+
         return sqlite3.connect('ahorcado.db')
 
+    #Metodo para obtener los jugadores
+
     def obtener_jugador(self, nombre):
+
         # Obtiene un jugador de la base de datos o lo crea si no existe.
+
         conn = self.conectar_db()
         cursor = conn.cursor()
         cursor.execute("SELECT * FROM Jugador WHERE nombre=?", (nombre,))
@@ -61,8 +71,12 @@ class InterfazAhorcado:
         conn.close()
         return jugador
 
+    #Metodo para elegir la palabra
+
     def elegir_palabra(self, tema):
+
         # Selecciona una palabra aleatoria del tema dado.
+
         conn = self.conectar_db()
         cursor = conn.cursor()
 
@@ -70,12 +84,15 @@ class InterfazAhorcado:
 
         print(f"Buscando palabras para el tema '{tema.strip()}'")
         # Realizar una consulta más simple
+
         cursor.execute("SELECT palabra FROM Palabra WHERE LOWER(tema) LIKE ?", ('%' + tema.lower() + '%',))
         resultados = cursor.fetchall()
 
         # Mostrar los resultados
+
         if resultados:
             # Selecciona una palabra aleatoria del tema
+
             palabra = random.choice(resultados)[0]  # obtener solo la palabra
 
             conn.close()
@@ -85,22 +102,29 @@ class InterfazAhorcado:
             conn.close()
             return None  # Devolver None si no se encuentran palabras
 
+    # Metodo para resetear el juego
+
     def reset(self):
+
         # Reinicia los valores del juego a su estado inicial
+
         self.palabra_oculta = []
         self.intentos = 6
         self.letras_intentadas = []
 
         # Obtén una nueva palabra del tema actual
+
         palabra = self.elegir_palabra(self.tema_actual)  # Usamos self.tema_actual para mantener el tema
         if palabra:
             self.palabra_original = palabra
             self.palabra_oculta = ['_'] * len(self.palabra_original)
 
             # Actualiza la interfaz del juego con la nueva palabra y los intentos
+
             self.actualizar_palabra_oculta()
 
             # Reinicia el estado del juego (por ejemplo, el contador de intentos)
+
             self.intentos_label.config(text=f"Te quedan {self.intentos} intentos.")
             self.letra_entry.delete(0, tk.END)  # Limpia el campo de la letra
             self.actualizar_imagen_ahorcado()
@@ -108,8 +132,11 @@ class InterfazAhorcado:
         else:
             messagebox.showwarning("Error", "No se pudo obtener una nueva palabra.")
 
+    #Metodo que ve la entrada del nombre
+
     def mostrar_entrada_nombre(self):
         # Función de validación usando expresiones regulares (re) que solo permite letras
+
         def validar_nombre(char):
             if re.match("^[A-Za-z]+$", char):  # Permite solo letras (mayúsculas o minúsculas)
                 return True
@@ -118,9 +145,11 @@ class InterfazAhorcado:
                 return False
 
         # Crear la validación del campo de entrada
+
         self.nombre_valida = self.root.register(validar_nombre)
 
         # Muestra la interfaz para ingresar el nombre del jugador.
+
         self.nombre_label = tk.Label(self.root, text="Ingrese su nombre:", bg="#f0f0f0", font=("Helvetica", 14))
         self.nombre_label.pack(pady=10)
 
@@ -143,8 +172,7 @@ class InterfazAhorcado:
                                        command=lambda: self.iniciar_juego('nombre de persona'))
         self.tema_button_3.pack(pady=5)
 
-
-
+    #Metodo que inicia el juego
 
     def iniciar_juego(self, tema):
         # Este método se llama cuando el jugador hace clic en un tema
@@ -161,16 +189,19 @@ class InterfazAhorcado:
             messagebox.showwarning("Error", f"No se encontraron palabras para el tema '{tema}'.")
             return
 
-        #Guardamos el tema para usarlo en el reset
-        self.tema_actual=tema
+        # Guardamos el tema para usarlo en el reset
+
+        self.tema_actual = tema
 
         # Inicializar los valores del juego
+
         self.palabra_oculta = ['_'] * len(palabra)
         self.palabra_original = palabra
         self.intentos = 6
         self.letras_intentadas = []
 
         # Ocultar la interfaz de inicio y mostrar la de juego
+
         self.nombre_label.pack_forget()
         self.nombre_entry.pack_forget()
         self.tema_label.pack_forget()
@@ -180,10 +211,11 @@ class InterfazAhorcado:
         self.boton_reset = tk.Button(self.root, text="Reset", font=("Helvetica", 14), command=self.reset)
         self.boton_reset.pack(pady=10)
 
-
-
         # Mostrar la interfaz de juego
+
         self.mostrar_juego()
+
+    #Metodo que muestra el jeugo
 
     def mostrar_juego(self):
         """Muestra la interfaz del juego."""
@@ -193,7 +225,7 @@ class InterfazAhorcado:
         self.intentos_label = tk.Label(self.root, text=f"Te quedan {self.intentos} intentos.", font=("Helvetica", 14))
         self.intentos_label.pack()
 
-        self.imagen_label= tk.Label(self.root)
+        self.imagen_label = tk.Label(self.root)
         self.imagen_label.pack(pady=10)
         self.actualizar_imagen_ahorcado()
 
@@ -205,29 +237,29 @@ class InterfazAhorcado:
         self.letra_entry.bind("<Key>", self.adivinar_letra)
         self.imagen_label.pack(pady=10)
 
-        self.salir_button=tk.Button( self.root)
+        self.salir_button = tk.Button(self.root)
 
-        self.estadisticas_button = tk.Button(self.root, text="Ver Estadísticas", font=("Helvetica", 14),command=self.mostrar_estadisticas)
+        self.estadisticas_button = tk.Button(self.root, text="Ver Estadísticas", font=("Helvetica", 14),
+                                             command=self.mostrar_estadisticas)
         self.estadisticas_button.pack()
 
         self.salir_button = tk.Button(self.root, text="Salir", font=("Helvetica", 14), command=self.salir)
         self.salir_button.pack(pady=10)
 
     def actualizar_palabra_oculta(self):
-            """Actualiza la visualización de la palabra oculta en la interfaz."""
-            self.palabra_label.config(text=" ".join(self.palabra_oculta))
-            self.intentos_label.config(text=f"Te quedan {self.intentos} intentos.")
+        #Actualiza la visualización de la palabra oculta en la interfaz.
 
-
-
-
+        self.palabra_label.config(text=" ".join(self.palabra_oculta))
+        self.intentos_label.config(text=f"Te quedan {self.intentos} intentos.")
 
     def salir(self):
-        """Cierra la aplicación."""
+        # Cierra la aplicación.
+
         self.root.quit()
 
     def actualizar_imagen_ahorcado(self):
-        """Actualiza la imagen del ahorcado según los intentos restantes."""
+        # Actualiza la imagen del ahorcado según los intentos restantes.
+
         if self.intentos == 6:
             self.imagen_label.config(image=self.imagenes[0])  # Sin fallos
         elif self.intentos == 5:
@@ -244,7 +276,9 @@ class InterfazAhorcado:
             self.imagen_label.config(image=self.imagenes[7])  # Mostrar imagen de derrota
 
     def adivinar_letra(self, event):
+
         # Deshabilitar el evento de la tecla si el juego terminó
+
         if self.intentos == 0 or "_" not in self.palabra_oculta:
             return  # Si el juego terminó, no se procesan más letras
 
@@ -276,6 +310,7 @@ class InterfazAhorcado:
                 self.letra_entry.config(state="disabled")  # Deshabilita la entrada de texto
 
         # Comprobamos si se ha ganado (todas las letras han sido adivinadas)
+
         if "_" not in self.palabra_oculta:
             self.imagen_label.config(image=self.imagenes[6])  # Muestra la imagen de la victoria
             messagebox.showinfo("¡Ganaste!", "¡Felicidades, adivinaste la palabra!")
@@ -283,7 +318,7 @@ class InterfazAhorcado:
             self.letra_entry.config(state="disabled")  # Deshabilita la entrada de texto
 
     def mostrar_estadisticas(self):
-        """Muestra las estadísticas del jugador en la interfaz gráfica."""
+        # Muestra las estadísticas del jugador en la interfaz gráfica.
         conn = self.conectar_db()
         cursor = conn.cursor()
         cursor.execute("SELECT victorias, derrotas, partidas_totales FROM Jugador WHERE id_jugador = ?",
@@ -294,10 +329,8 @@ class InterfazAhorcado:
         stats_text = f"Victorias: {victorias}\nDerrotas: {derrotas}\nPartidas Totales: {partidas_totales}"
         messagebox.showinfo("Estadísticas", stats_text)
 
-
-
     def actualizar_estadisticas(self, gano):
-        """Actualiza las estadísticas del jugador en la base de datos."""
+        # Actualiza las estadísticas del jugador en la base de datos.
         conn = self.conectar_db()
         cursor = conn.cursor()
 
@@ -315,6 +348,7 @@ class InterfazAhorcado:
 
 
 # Crear la ventana principal
+
 root = tk.Tk()
 root.config(bg="#ebdae4")
 juego = InterfazAhorcado(root)
